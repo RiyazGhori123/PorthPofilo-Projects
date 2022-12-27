cb11ea9731fffdabcb68ee2495484ee6ee23330c
Shaik Riyaz Ghori <90542870+RiyazGhori123@users.noreply.github.com>
2022-12-27 17:02:19 +0530

    Add files via upload
    Updated Query

--- PORTFOLIO PROJECT 1 ON COVID DATA.sql
+++ PORTFOLIO PROJECT 1 ON COVID DATA.sql
@@ -1,63 +1,182 @@
---SELECT *
---FROM [PORTFOLIO PROJECT].[dbo].[CovidDeaths$]
+/*
+Queries used for Tableau Project
+*/
 
---SELECT location,date, total_cases, new_cases, total_deaths, population_density
---FROM CovidDeaths$
---ORDER BY 1,2
 
--- Looking at Total Cases vs Total Deaths
 
---SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
---FROM CovidDeaths$
---WHERE location like '%states%'
---Order by 1,2 
+-- 1. 
 
--- Looking at Total cases vs Population
--- Shows what percentage of Population got covid
---SELECT Location, date, total_cases, population_density, (total_cases/population_density)*100 as PoupulationPercentage
---FROM CovidDeaths$
---WHERE location like '%india%'
---Order by 1,2 
+SELECT * 
+FROM [PORTFOLIO PROJECT].DBO.CovidDeaths$
 
--- Looking at Countries with Highest Infection Rate Compared to Population
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
 
---SELECT Location, population_density, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population_density))*100 as PercentagePopulationInfected
---FROM CovidDeaths$
---WHERE location like '%india%'
---Group by location, population_density
---Order by PercentagePopulationInfected DESC
+-- Just a double check based off the data provided
+-- numbers are extremely close so we will keep them - The Second includes "International"  Location
 
--- Showing Countries with Highest Death Count per Population
 
---SELECT location, Max(cast(total_deaths as int)) as TotalDeathCount
---FROM [PORTFOLIO PROJECT].dbo.CovidDeaths$
-----WHERE LOCATION LIKE '%INDIA%'
---GROUP BY LOCATION
---ORDER BY TotalDeathCount DESC
-
--- LET'S BREAKS THINGS DWON BY CONTINENT
-
---SELECT continent, Max(cast(total_deaths as int)) as TotalDeathCount
---FROM [PORTFOLIO PROJECT].dbo.CovidDeaths$
-----WHERE LOCATION LIKE '%INDIA%'
---WHERE continent is Not NULL
---GROUP BY continent
---ORDER BY TotalDeathCount DESC
-
---- CONTINENTS IWTH HIGHEST DEATH COUNT PER POPULATION same as above
-
---GLOBAL NUMBERS
---SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_cases)*100 as DeathPercentage
---FROM [PORTFOLIO PROJECT].DBO.CovidDeaths$
---WHERE continent is NOT NULL
+--Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+--From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+----Where location like '%states%'
+--where location = 'World'
+----Group By date
 --order by 1,2
 
-SELECT dea.continent, dea.location, dea.date,
-      dea.population_density, vac.new_vaccinations, sum(convert(int,vac.new_vaccinations )) OVER (Partition by dea.Location)
-FROM [PORTFOLIO PROJECT].DBO.[CovidVaccinations$'] VAC
-Join [PORTFOLIO PROJECT].DBO.[CovidDeaths$] DEA
-    On dea.location = vac.location
-	and dea.date = vac.date
-WHERE dea.continent is NOT NULL
-order by 2,3
 
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, population_density, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population_density))*100 as PercentPopulationInfected
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+Group by Location, population_density
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, population_density,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population_density))*100 as PercentPopulationInfected
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+Group by Location, population_density, date
+order by PercentPopulationInfected desc
+
+
+
+
+
+
+
+
+
+
+
+
+-- Queries I originally had, but excluded some because it created too long of video
+-- Here only in case you want to check them out
+
+
+-- 1.
+
+Select dea.continent, dea.location, dea.date, dea.population_density
+, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$ dea
+Join [PORTFOLIO PROJECT].DBO.[CovidVaccinations$'] vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+group by dea.continent, dea.location, dea.date, dea.population_density
+order by 1,2,3
+
+
+
+
+-- 2.
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+-- Just a double check based off the data provided
+-- numbers are extremely close so we will keep them - The Second includes "International"  Location
+
+
+--Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+--From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+----Where location like '%states%'
+--where location = 'World'
+----Group By date
+--order by 1,2
+
+
+-- 3.
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+
+-- 4.
+
+Select Location, population_density, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population_density))*100 as PercentPopulationInfected
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+Group by Location, population_density
+order by PercentPopulationInfected desc
+
+
+
+-- 5.
+
+--Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+--From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+----Where location like '%states%'
+--where continent is not null 
+--order by 1,2
+
+-- took the above query and added population
+Select Location, date, population_density, total_cases, total_deaths
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+where continent is not null 
+order by 1,2
+
+
+-- 6. 
+
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population_density, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$ dea
+Join [PORTFOLIO PROJECT].DBO.[CovidVaccinations$'] vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100 as PercentPeopleVaccinated
+From PopvsVac
+
+
+-- 7. 
+
+Select Location, population_density,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population_density))*100 as PercentPopulationInfected
+From [PORTFOLIO PROJECT].DBO.CovidDeaths$
+--Where location like '%states%'
+Group by Location, population_density, date
+order by PercentPopulationInfected desc
